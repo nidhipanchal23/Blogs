624580b0400e2c70d98b528ffc8c2e58994bdbcc
nidhipanchal23 <112480138+nidhipanchal23@users.noreply.github.com>
2023-05-17 17:48:57 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
     },
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
