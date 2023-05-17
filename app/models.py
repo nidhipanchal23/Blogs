65a71ce01703f25c699738ebf10ed66f6731d40d
nidhipanchal23 <112480138+nidhipanchal23@users.noreply.github.com>
2023-05-17 10:56:44 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -73,11 +73,11 @@ class User(AbstractBaseUser):
         # Simplest possible answer: All admins are staff
         return self.is_superuser
    
-    class MyModel(models.Model):
-        name = models.CharField(max_length=100)
-        age = models.IntegerField()
-        email = models.EmailField()
-        created_at = models.DateTimeField(auto_now_add=True)
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
