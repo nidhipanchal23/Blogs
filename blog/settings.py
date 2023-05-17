461830e8134a79ab6efe089556a0a78fba2a3ac6
nidhipanchal23 <112480138+nidhipanchal23@users.noreply.github.com>
2023-05-17 17:52:13 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
