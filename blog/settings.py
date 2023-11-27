f20e55dc8a37eb1c4356b34a7596b4d0f8c977e5
nidhi panchal <nidhispanchal23@gmail.com>
2023-11-27 17:19:01 +0530

    [CHANGES] searching filter

--- blog/settings.py
+++ blog/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'new_blogs', 
+        'NAME': 'blogs', 
         'USER': 'postgres', 
-        'PASSWORD': 'ct##123456',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     },
